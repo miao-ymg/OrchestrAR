012c4eab96e55bc679573563b5ee1c0fbd647c8d
David Stiftl <82659221+stiftlD@users.noreply.github.com>
2022-06-29 17:15:30 +0200

    add marker tracker header

---
+++ include/MarkerTracker.h
@@ -0,0 +1,49 @@
+#ifndef MARKER_TRACKER_H
+#define MARKER_TRACKER_H
+
+#include <opencv/cv.h>
+
+class CvMemStorage;
+
+const std::string kWinName1 = "Exercise 8 - Original Image";
+const std::string kWinName2 = "Exercise 8 - Converted Image";
+const std::string kWinName3 = "Exercise 8 - Stripe Image";
+const std::string kWinName4 = "Exercise 8 - Marker";
+
+class MarkerTracker{
+public:
+	MarkerTracker(double kMarkerSize_) 
+		:	thresh(100),
+		bw_thresh(100),
+		kMarkerSize(kMarkerSize_)
+	{
+		init();
+	}
+	MarkerTracker(double kMarkerSize_, int thresh_, int bw_thresh_) 
+		:	thresh(thresh_),
+		bw_thresh(bw_thresh_),
+		kMarkerSize(kMarkerSize_)
+	{
+		init();
+	}
+	~MarkerTracker(){
+		cleanup();
+	}
+	void findMarker( cv::Mat &img_bgr, float resultMatrix[16] );
+protected:
+	void init( );
+	void cleanup( );
+
+	//camera settings
+	const double kMarkerSize; // Marker size [m]
+
+	cv::Mat img_gray;
+	cv::Mat img_mono;
+
+	int thresh; // Threshold (gray to mono)
+	int bw_thresh; // threshold for (gray maker to ID image)
+
+	CvMemStorage* memStorage;
+};
+
+#endif // MARKER_TRACKER_H
